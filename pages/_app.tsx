b1b32c3a7a98e8dae464e3520548453f15ebfcb7
nabil-rady <midorady9999@gmail.com>
2023-03-14 14:51:16 +0200

    Add Built-in SASS support.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "../styles/style.css";
+import "../styles/style.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
