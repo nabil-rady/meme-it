70069a35a6f6525aceb3a7c49c65032774ac101f
Youssef Ahmed <youssefashwal@outlook.com>
2023-03-16 17:59:53 +0200

    Added react-toastify for pop-up notifications.

--- components/Invite.tsx
+++ components/Invite.tsx
@@ -1,18 +1,35 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 interface InviteProps {
   id: string;
 }
 
 export default function Invite(props: InviteProps) {
   const origin = window.location.origin;
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(`${origin}/${props.id}`);
+
+    toast("Link Copied", {
+      position: "bottom-center",
+      toastId: "pop-up",
+      theme: "dark",
+      hideProgressBar: true,
+      closeButton: false,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: false,
+      autoClose: 1000,
+    });
+  };
   return (
     <div className="invite">
       <div className="invite-content">Invite your friends</div>
-      <button
-        className="button"
-        onClick={() => navigator.clipboard.writeText(`${origin}/${props.id}`)}
-      >
+      <button className="button" onClick={copyLink}>
         Copy
       </button>
+      <ToastContainer className="pop-up-container" pauseOnFocusLoss={false} />
     </div>
   );
 }
