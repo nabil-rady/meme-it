345a003c35aa0c5f33350f776f24797f75cf26a3
Youssef Ahmed <youssefashwal@outlook.com>
2023-03-22 02:42:02 +0200

    Added avatar selection functionality.

---
+++ components/AvatarSelect.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { IoMdClose } from "react-icons/io";
+
+interface AvatarProps {
+  avatar: string;
+  closeAvatarSelect: () => void;
+  changeAvatar: (src: string) => void;
+}
+
+export default function AvatarSelect({
+  avatar,
+  closeAvatarSelect,
+  changeAvatar,
+}: AvatarProps) {
+  const Avatars = Array.from(Array(12).keys()).map((n: number) => {
+    const link = `/avatars/${n + 1}.jpg`;
+    const size = "96";
+
+    return (
+      <Image
+        src={link}
+        alt=""
+        width={size}
+        height={size}
+        className={`avatar ${avatar === link ? "selected" : ""}`}
+        onClick={() => changeAvatar(link)}
+      />
+    );
+  });
+
+  return (
+    <div className="avatar-select-overlay" onClick={closeAvatarSelect}>
+      <div className="avatar-select-body" onClick={(e) => e.stopPropagation()}>
+        <h2>pick an avatar</h2>
+        <div className="avatars">{Avatars}</div>
+        <button onClick={closeAvatarSelect}>
+          <IoMdClose size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { BiRefresh } from "react-icons/bi";
 
-import Avatar from "../public/avatar/bugs.jpg";
-
 import Player from "../components/Player";
 import Invite from "../components/Invite";
+import AvatarSelect from "../components/AvatarSelect";
 
 import handleResponse from "../lib/handleResponse";
 
@@ -19,11 +18,25 @@ import {
 
 export default function Home() {
   const ws = useRef<WebSocket>();
-  const nicknameInput = useRef<HTMLInputElement>(null);
 
   const [thisPlayer, setThisPlayer] = useState<PlayerInfo>();
   const [players, setPlayers] = useState<PlayerInfo[]>([]);
+  const [avatar, setAvatar] = useState<string>("/avatars/1.jpg");
+  const [nickname, setNickname] = useState<string>("");
   const [game, setGame] = useState<GameInfo>();
+  const [showAvatarSelect, setShowAvatarSelect] = useState<Boolean>(false);
+
+  const openAvatarSelct = (): void => {
+    setShowAvatarSelect(true);
+  };
+
+  const closeAvatarSelect = (): void => {
+    setShowAvatarSelect(false);
+  };
+
+  const changeAvatar = (src: string): void => {
+    setAvatar(src);
+  };
 
   const createGame = (nickname: string, avatar: string) => {
     const hostname = window.location.hostname;
@@ -56,31 +69,40 @@ export default function Home() {
   const renderHome = () => (
     <>
       <div className="avatar-container">
-        <Image src={Avatar} alt="avatar" className="avatar" priority />
-        <button className="change-avatar">
+        <Image
+          src={avatar}
+          alt="avatar"
+          className="avatar"
+          width={585}
+          height={585}
+          onClick={openAvatarSelct}
+          priority
+        />
+        <button className="change-avatar" onClick={openAvatarSelct}>
           <BiRefresh className="icon" />
         </button>
       </div>
 
       <div className="nickname-container">
         <input
-          ref={nicknameInput}
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
           className="nickname-input"
           placeholder="Nickname"
         />
       </div>
 
+      {showAvatarSelect && (
+        <AvatarSelect
+          avatar={avatar}
+          closeAvatarSelect={closeAvatarSelect}
+          changeAvatar={changeAvatar}
+        />
+      )}
+
       <div className="buttons">
         <button className="button">Join Lobby</button>
-        <button
-          className="button"
-          onClick={() =>
-            createGame(
-              nicknameInput.current?.value as string,
-              "/avatar/bugs.jpg"
-            )
-          }
-        >
+        <button className="button" onClick={() => createGame(nickname, avatar)}>
           Create New Lobby
         </button>
       </div>
